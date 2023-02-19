2ad8473c2f738f9440e5c2b7b69892bb0421415b
godfrey <gbespanol@gmail.com>
2023-02-20 00:25:35 +0800

    third commit

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -16,7 +16,7 @@ class AdminController extends Controller
 
         $request->session()->regenerateToken();
 
-        return redirect('/login');
+        return redirect('/');
         //end method
     }
 }
--- resources/views/admin/body/sidebar.blade.php
+++ resources/views/admin/body/sidebar.blade.php
@@ -17,7 +17,7 @@
         <div id="sidebar-menu">
             <!-- Left Menu Start -->
             <ul class="metismenu list-unstyled" id="side-menu">
-                <li class="menu-title">Menu</li>
+                <li class="menu-title">UPCEB-IMS</li>
 
                 <li>
                     <a href="index.html" class="waves-effect">
